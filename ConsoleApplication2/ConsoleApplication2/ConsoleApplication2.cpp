71b49f66c7f329d037765125f1ec6b78dcc45cf5
HAFlorianGP <55873217+HAFlorianGP@users.noreply.github.com>
2021-09-23 15:32:00 +0200

    struct Vec + & /

--- ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -5,7 +5,9 @@
 #include <iostream>
 #include "Toto.h"
 
-int main()
+
+//--------------------------------------------- 1er exo --------------------------
+/*int main()
 {
 	printf("0\n");
 
@@ -19,25 +21,101 @@ int main()
 	printf("%lli\n", 132456789);
 
 	std::cout << "123465789" << "\n";
+}*/
+
+//-------------------------------------------------------Pointeur sur Tab------------------------
+	/*int main()
+	{
+		int tab[] = { 66,1,2,3 };
+		int* c = &tab[0];
+		*c = 0;
+
+		//printf("%d\n", *c++);
+		//afficher c  incremente c et prend le contenu
+
+		//printf("%d\n", (*c)++);
+		//prend le contenu de c et incrémente le 
+
+		//printf("%d\n", *++c);
+		// incremente c et prend le contenu 
+
+		//printf("%d\n", ++*c);
+		// prend le contenu de c et incremente le 
+	}*/
+
+
+//--------------------------------------------------------------- Struct manipulation Pointeur  exple-------------------------------------------
+/*struct Vec {
+
+	float x = 0;
+	float y = 0;
+	float z = 0;
+};
+
+int main() {
+	//Vec toto;
+
+	Vec* toto = new Vec();
+	toto->y = 67;
+	(*toto).x = 69;
+	printf("%f %f %f", toto->x, toto->y, toto->z);
+};*/
+
+struct Vec4 {
+
+	float x = 0;
+	float y = 0;
+	float z = 0;
+	float w = 0;
+
+	Vec4 add(Vec4 A ) {
+
+		Vec4 res;
+
+		res.x = x + A.x;
+		res.y = y + A.y;
+		res.z = z + A.z;
+		res.w = w + A.w;
+		
+		return res;
+	}
+
+	Vec4 div(Vec4 A) {
+
+		Vec4 res;
+
+		res.x = x / A.x;
+		res.y = y / A.y;
+		res.z = z / A.z;
+		res.w = w / A.w;
+
+		return res;
+	}
+
+
+};
+
+int main() {
+
+	Vec4* toto = new Vec4;
+	Vec4* tata = new Vec4;
+
+	toto->x = 1;
+	toto->y = 1;
+	toto->z = 1;
+	toto->w = 1;
+
+	tata->x = 1;
+	tata->y = 1;
+	tata->z = 1;
+	tata->w = 5;
+
+	//Vec4 res = toto->add(*tata);
+	Vec4 res = toto->div(*tata);
+	printf("%f\n %f\n %f\n %f\n", res.x, res.y, res.z, res.w);
 }
 
-int main0()
-{
-	printf("0\n");
 
-	//allocation static
-	// L'instance est sur la pile /heap
-
-	Toto tStatic;
-	printf("%f\n", tStatic.x);
-	// garde pour plus tard
-	// allocation dynamique
-	//l'instance est sur le tas / heap
-
-	//Toto* tDynamic = new Toto();
-
-	return 0;
-}
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
