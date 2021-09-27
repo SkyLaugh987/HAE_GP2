a140dfc79e5bd2f2540ce564f73904ea9f117ec1
SkyLaugh987 <skylaughblue@gmail.com>
2021-09-27 17:08:08 +0200

    ui

--- ConsoleRévision/ConsoleRévision/ConsoleRévision.cpp
+++ ConsoleRévision/ConsoleRévision/ConsoleRévision.cpp
@@ -5,28 +5,194 @@
 #include <iostream>
 #include "Tata.h"
 
-/*int main0()
+//#include "IntArray.h"
+
+
+//--------------------------------------------- 1er exo --------------------------
+/*int main()
 {
-	printf("%i\n", 0);
-	printf("%f\n", 0.5);
+	printf("0\n");
+
+	int zero = 0;
+	printf("%i\n", zero);
+
+	float decimal = 0.5f;
+	printf("%f\n", decimal);
+
 	printf("%s\n", "toto");
-	printf("%lli\n", 12345678910);
+	printf("%lli\n", 132456789);
+
+	std::cout << "123465789" << "\n";
 }*/
+
+//-------------------------------------------------------Pointeur sur Tab------------------------
+	/*int main()
+	{
+		int tab[] = { 66,1,2,3 };
+		int* c = &tab[0];
+		*c = 0;
+
+		//printf("%d\n", *c++);
+		//afficher c  incremente c et prend le contenu
+
+		//printf("%d\n", (*c)++);
+		//prend le contenu de c et incrémente le
+
+		//printf("%d\n", *++c);
+		// incremente c et prend le contenu
+
+		//printf("%d\n", ++*c);
+		// prend le contenu de c et incremente le
+	}*/
+
+
+	//--------------------------------------------------------------- Struct manipulation Pointeur  exple-------------------------------------------
+	/*struct Vec {
+
+		float x = 0;
+		float y = 0;
+		float z = 0;
+	};
+
+	int main() {
+		//Vec toto;
+
+		Vec* toto = new Vec();
+		toto->y = 67;
+		(*toto).x = 69;
+		printf("%f %f %f", toto->x, toto->y, toto->z);
+	};*/
+	//-------------------------------------------------------------------------- Struct V4  fonction-------------------------
+struct Vec4 {
+
+	float x = 0;
+	float y = 0;
+	float z = 0;
+	float w = 0;
+
+	Vec4 add(Vec4 A) {
+
+		Vec4 res;
+
+		res.x = x + A.x;
+		res.y = y + A.y;
+		res.z = z + A.z;
+		res.w = w + A.w;
+
+		return res;
+	}
+
+	Vec4 add(float f) {
+
+		Vec4 res;
+
+		res.x = x + f;
+		res.y = y + f;
+		res.z = z + f;
+		res.w = w + f;
+
+		return res;
+	}
+
+	Vec4 div(Vec4 A) {
+
+		Vec4 res;
+
+		res.x = x / A.x;
+		res.y = y / A.y;
+		res.z = z / A.z;
+		res.w = w / A.w;
+
+		return res;
+	}
+
+
+};
+
+/*int main() {
+
+	Vec4* toto = new Vec4;
+	Vec4* tata = new Vec4;
+
+	toto->x = 1;
+	toto->y = 1;
+	toto->z = 1;
+	toto->w = 1;
+
+	tata->x = 1;
+	tata->y = 1;
+	tata->z = 1;
+	tata->w = 5;
+
+	//Vec4 res = toto->add(*tata);
+	Vec4 res = toto->div(*tata);
+	printf("%f\n %f\n %f\n %f\n", res.x, res.y, res.z, res.w);
+}*/
+
+
+//----------------------------------------------------------------- Exo IntArray----------------------------------------------
+
+/*int main() {
+
+	int* bobData = nullptr;
+
+	IntArray bob(10);
+
+	for (int i = 0; i < 10; i++) {
+		bob.set(i, i * i);
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", bob.get(i));
+	}
+	printf("\n");
+
+	//bob.set(20, 10);
+
+	return 0;
+}*/
+
+//-------------------------------------------------------------------CHAR------------------------------------------
+
+
+int Strlen(const char* maChaine) {
+
+	//declarer le resultat
+
+	// parcourir la chaine
+	while (true) {
+		//avancer mo,n index
+	}
+	//si on trouve le character == 0
+
+
+}
+int Countc(const char* maChaine, char c) {
+
+
+	// renvoie le nombre d'occurence de c dans machaine 
+
+	// machaine est un tableau de char
+
+	// si l'element de l'idx courant = c 
+
+	// inc le resultat
+
+	// renvoyer le resultat
+
+	int res = 0;
+	int stringLength = strlen(maChaine);
+
+	for (int i = 0; i < (stringLength); i++) {
+		if (maChaine[i] == c) {
+			res++;
+		};
+	}
+	return res;
+}
 
 int main() {
 
-	Tata tStatic;
-	printf("%f\n", tStatic.x);
-	//printf("%s\n", "aaaaaaaaaaaah");
+	int nbA = Countc("sapin", 'a');
+	printf("%d", nbA);
+
 }
-
-// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
-// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
-
-// Conseils pour bien démarrer : 
-//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
-//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
-//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
-//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
-//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
-//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
---
+++ ConsoleRévision/ConsoleRévision/IntArray.h
@@ -0,0 +1,45 @@
+#pragma once
+class IntArray {
+	int* data = nullptr;
+	int size = 0;
+
+public:
+
+
+	IntArray(int size) {
+		//faire l'allocation dynamique de "data" qui sera de taille "suffisement grande"
+		data = new int[size];
+		this->size = size;
+
+	};
+
+	void set(int idx, int value) {
+
+		// récupérer la donnée à la case idx et l'affecter
+		// si idx hors des bornes du tab -> "exeption: out of bounds
+
+		if ((idx > size || idx < size)) {
+			throw "exeption out of bounds";
+		}
+		else {
+			data[idx] = value;
+		}
+	};
+
+	int get(int idx) {
+
+		// récupérer la donnée à la case idx et la retourner
+		if ((idx > size) || (idx < size)) {
+			throw "exeption out of bounds";
+		}
+		else {
+			return data[idx];
+		}
+	};
+
+	~IntArray() {
+
+		delete[] data;
+	}
+
+};
