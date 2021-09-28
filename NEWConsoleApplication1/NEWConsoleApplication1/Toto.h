ab7871507a96f456d35295339d250325591544ac
Isshinaru <arsene02031991@gmail.com>
2021-09-28 13:47:58 +0200

    NEWConsole1

---
+++ NEWConsoleApplication1/NEWConsoleApplication1/Toto.h
@@ -0,0 +1,16 @@
+#pragma once
+class A
+{
+
+};
+struct Toto : public A
+{
+public:
+	float x = 0.0f;
+};
+
+template<typename MaClasse>
+class Foo {
+	MaClasse* ptr = nullptr;
+};
+typedef Foo<float> B;
