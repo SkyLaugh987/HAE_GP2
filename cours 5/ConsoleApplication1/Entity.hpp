d8cb4ee231de7b4acf4fa471b5619c8eeeba9a8c
Isshinaru <arsene02031991@gmail.com>
2021-11-09 17:30:09 +0100

    entity class

---
+++ cours 5/ConsoleApplication1/Entity.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/CircleShape.hpp"
+#include "SFML//Graphics/Sprite.hpp"
+
+class Entity {
+public:
+	sf::Shape sprite;
+};
